977df5e0079bb5c5c2e2c59a5f05a59c06814717
Nathan Perry <nathan@salsadigital.com.au>
2019-06-18 18:35:16 +1200

    [DQL-43] YTP Comments - QA remediation items (#8)
    * [DQL-25] Add comment flagging functionality.
    
    * [DQL-25] Add basic javascript validation to comments.
    
    * [DQL-25] Add profanity checking to comments.
    
    * Add flagged value to update action.
    
    * [DQL-25] Updated bad words list.
    
    * [DQL-25] Extend YTP to support comments on Data Requests.
    
    * [DQL-25] Redirect to original page on error.
    
    * [DQL-25] Re-use existing redirect URL function.
    
    * [DQL-25] Redirect delete action to comment ID.
    
    * [DQL-25] Email notifications.
    
    * Added _ to imports.
    
    * Include `lxml` as requirement
    
    * [DQL-43] Threaded comments POC.
    
    * [DQL-25] Adjust notifications to include datarequests.
    
    * [DQL-25] [DQL-43] Comments
    - Move non-Data.Qld specific comment helper functions into YTP extension
    - Highlight comment set in URL anchor/fragment identifier
    - Additional changes for comment threading
    - Additional changes to support commenting on Data Requests
    
    * [DQL-25] Refactor and tidy up code.
    
    * [DQL-25] Refactor notification code and fix URL for data requests comment page in email.
    
    * [DQL-43] Threaded comment related updates
    - separate email notifications for new comment and reply comment
    - add functionality to get primary commenter, and other commenters for a comment thread
    - add `parent_id` to comment data_dict on creation
    
    * [DQL-25] Fix typo when checking for content type.
    
    * [DQL-25] Sprint 4 demo feedback.
    
    * [DQL-25] Fixed gap found in processing comments full of whitespace chars.
    
    * [DQL-43] Demo feedback - deleted users not linked in comment output.
    
    * [DQL-43] Sprint demo 4 feedback.
    
    * [DQL-25] Change profanity library to profanityfilter.
    
    * [DQL-43] Bugfix: Org admin cannot delete comment.

--- ckanext/ytp/comments/logic/auth/delete.py
+++ ckanext/ytp/comments/logic/auth/delete.py
@@ -5,6 +5,8 @@ import ckan.authz as authz
 from ckan import logic
 import ckanext.ytp.comments.model as comment_model
 
+from ckanext.ytp.comments import helpers
+
 
 log = logging.getLogger(__name__)
 
@@ -23,6 +25,15 @@ def comment_delete(context, data_dict):
     comment = comment_model.Comment.get(cid)
     if not comment:
         return {'success': False, 'msg': _('Comment does not exist')}
+
+    content_type = data_dict.get('content_type', None)
+    content_item_id = data_dict.get('content_item_id', None)
+
+    if content_type and content_item_id:
+        if not helpers.user_can_manage_comments(content_type, content_item_id):
+            return {'success': False, 'msg': _('User is not authorised to delete this comment')}
+        else:
+            return {'success': True}
 
     if comment.user_id is not userobj.id:
         return {'success': False, 'msg': _('User is not the author of the comment')}
