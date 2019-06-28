fd08b389aff37b70046b4c428f87a2acf6f4aa78
Nathan Perry <nathan@salsadigital.com.au>
2019-06-28 15:36:53 +1200

    [DQL-25] Bugfix: allow org admins to update comments.

--- ckanext/ytp/comments/logic/auth/update.py
+++ ckanext/ytp/comments/logic/auth/update.py
@@ -3,6 +3,8 @@ from ckan.common import _
 from ckan import logic
 import ckanext.ytp.comments.model as comment_model
 
+from ckanext.ytp.comments import helpers
+
 log = logging.getLogger(__name__)
 
 
@@ -21,6 +23,15 @@ def comment_update(context, data_dict):
     comment = comment_model.Comment.get(cid)
     if not comment:
         return {'success': False, 'msg': _('Comment does not exist')}
+
+    content_type = data_dict.get('content_type', None)
+    content_item_id = data_dict.get('content_item_id', None)
+
+    if content_type and content_item_id:
+        if not helpers.user_can_manage_comments(content_type, content_item_id):
+            return {'success': False, 'msg': _('User is not authorised to delete this comment')}
+        else:
+            return {'success': True}
 
     if comment.user_id != userobj.id:
         return {'success': False, 'msg': _('User is not the author of the comment')}
