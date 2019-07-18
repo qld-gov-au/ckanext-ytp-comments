5612b251bd736dbcf37d71715e26aa6668843930
Nathan Perry <nathan@salsadigital.com.au>
2019-07-19 08:39:15 +1200

    [DQL-113] Hide edit form when cancel button clicked.

--- ckanext/ytp/comments/public/javascript/comments.js
+++ ckanext/ytp/comments/public/javascript/comments.js
@@ -1,3 +1,13 @@
 function ShowCommentForm(id){
     $("#" + id).removeClass('hidden');
 }
+
+jQuery(document).ready(function() {
+
+    jQuery('.comment-container form .btn-cancel').on('click', function(e) {
+        var form = jQuery(this).closest('form');
+        if (form) {
+            form.addClass('hidden');
+        }
+    });
+});
