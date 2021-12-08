b4effeae78ef89589867634f34c5f4a3b2f8d01f
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-12-08 14:18:56 +1000

    [QOL-8368] enable POST requests on Flask routes

--- ckanext/ytp/comments/controllers/blueprints.py
+++ ckanext/ytp/comments/controllers/blueprints.py
@@ -14,19 +14,19 @@ commenting = Blueprint(
 
 if helpers.show_comments_tab_page():
     commenting.add_url_rule('/dataset/<dataset_id>/comments', 'dataset_comments', view_func=dataset_comments)
-commenting.add_url_rule('/<dataset_id>/comments/add', view_func=add)
-commenting.add_url_rule('/<content_type>/<dataset_id>/comments/add', view_func=add)
-commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/edit', view_func=edit)
-commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/edit', view_func=edit)
-commenting.add_url_rule('/<dataset_id>/comments/<parent_id>/reply', view_func=reply)
-commenting.add_url_rule('/<content_type>/<dataset_id>/comments/<parent_id>/reply', view_func=reply)
-commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/delete', view_func=delete)
-commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/delete', view_func=delete)
-commenting.add_url_rule('/comment/<comment_id>/flag', view_func=flag)
-commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/unflag', view_func=unflag)
-commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/unflag', view_func=unflag)
-commenting.add_url_rule('/comments/<thread_or_comment_id>/follow', view_func=follow)
-commenting.add_url_rule('/comments/<thread_or_comment_id>/mute', view_func=mute)
+commenting.add_url_rule('/<dataset_id>/comments/add', view_func=add, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_type>/<dataset_id>/comments/add', view_func=add, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/edit', view_func=edit, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/edit', view_func=edit, methods=('GET', 'POST'))
+commenting.add_url_rule('/<dataset_id>/comments/<parent_id>/reply', view_func=reply, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_type>/<dataset_id>/comments/<parent_id>/reply', view_func=reply, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/delete', view_func=delete, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/delete', view_func=delete, methods=('GET', 'POST'))
+commenting.add_url_rule('/comment/<comment_id>/flag', view_func=flag, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_item_id>/comments/<comment_id>/unflag', view_func=unflag, methods=('GET', 'POST'))
+commenting.add_url_rule('/<content_type>/<content_item_id>/comments/<comment_id>/unflag', view_func=unflag, methods=('GET', 'POST'))
+commenting.add_url_rule('/comments/<thread_or_comment_id>/follow', view_func=follow, methods=('GET', 'POST'))
+commenting.add_url_rule('/comments/<thread_or_comment_id>/mute', view_func=mute, methods=('GET', 'POST'))
 
 
 def get_blueprints():
