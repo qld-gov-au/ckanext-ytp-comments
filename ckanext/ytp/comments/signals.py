e6bf59f3b36a465962facea4a63207c124b6ed98
william dutton <will.dutt@gmail.com>
2023-07-10 13:31:07 +1000

    CHDEXCHAE-64 only enable signals on 2.10+ ckan

--- ckanext/ytp/comments/signals.py
+++ ckanext/ytp/comments/signals.py
@@ -1,29 +1,32 @@
+import logging
+
+log = logging.getLogger(__name__)
+
 try:
     import ckan.plugins.toolkit as tk
 
     ckanext = tk.signals.ckanext
+
+    created = ckanext.signal("comments:created")
+    """Sent when a new comment created.
+    Params:
+        sender: Thread ID
+        comment: comment dictionary
+    """
+
+    updated = ckanext.signal("comments:updated")
+    """Sent after an update of exisning comment.
+    Params:
+        sender: Thread ID
+        comment: comment dictionary
+    """
+
+    deleted = ckanext.signal("comments:deleted")
+    """Sent when a comment is deleted.
+    Params:
+        sender: Thread ID
+        comment: comment dictionary
+    """
+
 except AttributeError:
-    from blinker import Namespace
-
-    ckanext = Namespace()
-
-created = ckanext.signal("comments:created")
-"""Sent when a new comment created.
-Params:
-    sender: Thread ID
-    comment: comment dictionary
-"""
-
-updated = ckanext.signal("comments:updated")
-"""Sent after an update of exisning comment.
-Params:
-    sender: Thread ID
-    comment: comment dictionary
-"""
-
-deleted = ckanext.signal("comments:deleted")
-"""Sent when a comment is deleted.
-Params:
-    sender: Thread ID
-    comment: comment dictionary
-"""
+    log.info("YTP-Comments: Signals only available in CKAN 2.10+")
